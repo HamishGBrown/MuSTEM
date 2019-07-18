0b30eaf3492b555156ed6c5cadc916096eb36473
Hamish Galloway Brown <hamishgallowaybrown@gmail.com>
2019-07-18 10:04:41 -0700

    if(.not.) compiler bug workaround
    See https://github.com/HamishGBrown/MuSTEM/issues/17 for more detail

--- Source/s_qep_tem.f90
+++ Source/s_qep_tem.f90
@@ -126,7 +126,9 @@ subroutine qep_tem
         
     else
 #endif        
-    if(.not. load_grates) projected_potential = make_qep_grates(idum)
+    if(.not. load_grates) then
+    	projected_potential = make_qep_grates(idum)
+    endif
     call load_save_add_grates(idum,projected_potential,nopiy,nopix,n_qep_grates,n_slices,nt,nat_slice)
         
 #ifdef GPU        
@@ -211,7 +213,9 @@ subroutine qep_tem
     
 #ifdef GPU
 prop_d=prop
-if(.not.on_the_fly) transf_d = qep_grates
+if(.not.on_the_fly) then
+	transf_d = qep_grates
+endif
 psi_initial_d = psi_initial
         
 	! Set accumulators to zero
